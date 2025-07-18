a5688e6694d883a01234708b43592ea69e358575
Matthieu Scarset <m@matthieuscarset.com>
2025-07-18 10:53:30 +0200

    feat: Lint and simplify

--- src/config.py
+++ src/config.py
@@ -8,5 +8,6 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RAW_DATA_PATH = os.path.join(PROJECT_ROOT, "data", "raw")
 PROCESSED_DATA_PATH = os.path.join(PROJECT_ROOT, "data", "processed")
+MODELS_PATH = os.path.join(PROJECT_ROOT, "models")
 
 HOLD_OUT = 0.3
