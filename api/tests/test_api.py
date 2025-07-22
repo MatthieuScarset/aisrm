3fc91ad3f1fcf0b58b6557e1132362ba6e6d0ba0
Matthieu Scarset <m@matthieuscarset.com>
2025-07-22 17:09:01 +0200

    fix: Let container time to start

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -2,10 +2,12 @@
 
 import unittest
 import requests
+import time
 
 
 class TestApi(unittest.TestCase):
     def test_api_root(self):
+        time.sleep(15)
         url = "http://localhost:8500"
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
